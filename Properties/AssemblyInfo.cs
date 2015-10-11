5d15db5dbc85df315f2c3662acb8ade64caf41c7
crozone <crozone_xz@hotmail.com>
2015-10-12 03:27:49 +1100

    Fixed broken project certificate reference left over from example project

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MiniCube")]
+[assembly: AssemblyTitle("BallDash")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MiniCube")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyProduct("BallDash")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
