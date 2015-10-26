e63920f7e29d6c0aae1b3bc7fb1e974f70add0b3
crozone <crozone_xz@hotmail.com>
2015-10-27 01:18:38 +1100

    Added persistent high scores

--- MazeGame.cs
+++ MazeGame.cs
@@ -107,8 +107,8 @@ namespace Project {
                 else { // Use 1 white light only
                     Lights[0].LightColor = Color.White.ToColor4();
                     Lights[0].LightIntensity = 0.5f;
-                    Lights[0].LightPosition = new Vector3(CurrentLevel.Map.Width / 2f * CurrentLevel.Map.MapUnitWidth, 
-                        CurrentLevel.Map.Height* CurrentLevel.Map.MapUnitHeight / 2f, 
+                    Lights[0].LightPosition = new Vector3(CurrentLevel.Map.Width / 2f * CurrentLevel.Map.MapUnitWidth,
+                        CurrentLevel.Map.Height * CurrentLevel.Map.MapUnitHeight / 2f,
                         DefaultLightHeight);
                     Lights[1].LightColor = Color.Black.ToColor4();
                     Lights[2].LightColor = Color.Black.ToColor4();
@@ -143,7 +143,7 @@ namespace Project {
             Input.gestureRecognizer.ManipulationCompleted += OnManipulationCompleted;
 
             IsStarted = false;
-            
+
         }
 
         private void LoadLevels() {
@@ -183,8 +183,33 @@ namespace Project {
             base.LoadContent();
         }
 
-        private void SaveLevelScore(LevelStatus status, string levelId) {
-            // TODO: Save the level score to database of some kind
+        private void SaveLevelStatus(LevelStatus status, string levelId) {
+            var localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+            LevelStatus previousStatus = GetStartingLevelStatus(levelId);
+
+            if ((previousStatus.BestCollisions == null) ||
+                (status.Collisions < previousStatus.BestCollisions)) {
+                localSettings.Values[string.Format("HighScore_{0}_BestCollisions", levelId)] = status.Collisions;
+            }
+
+            if ((previousStatus.BestTime == null) ||
+                (status.Time < previousStatus.BestTime)) {
+                localSettings.Values[string.Format("HighScore_{0}_BestTime", levelId)] = (int)(status.Time.TotalMilliseconds + 0.5);
+            }
+        }
+
+        public LevelStatus GetStartingLevelStatus(string levelId) {
+            var localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+            var levelStatus = new LevelStatus();
+
+            int? bestTimeMs = localSettings.Values[string.Format("HighScore_{0}_BestTime", levelId)] as int?;
+            if (bestTimeMs != null) {
+                levelStatus.BestTime = new TimeSpan(0, 0, 0, 0, (int)bestTimeMs);
+            }
+
+            levelStatus.BestCollisions = localSettings.Values[string.Format("HighScore_{0}_BestCollisions", levelId)] as int?;
+
+            return levelStatus;
         }
 
         private void PlayerCompletedLevel(object sender, EventArgs args) {
@@ -193,7 +218,7 @@ namespace Project {
                 levelTimer.Stop();
                 TimeSpan levelTime = new TimeSpan(levelTimer.ElapsedTicks);
                 CurrentLevelStatus.Time = levelTime;
-                SaveLevelScore(CurrentLevelStatus, CurrentLevel.LevelID);
+                SaveLevelStatus(CurrentLevelStatus, CurrentLevel.LevelID);
 
 
                 // find the next level higher than this one and load it
@@ -227,7 +252,7 @@ namespace Project {
         }
 
         private void ChangeMap(Map map) {
-            lock(isStartedLock) {
+            lock (isStartedLock) {
                 // clear out all existing assets
                 GameObjects.Clear();
                 addedGameObjects.Clear();
@@ -281,13 +306,6 @@ namespace Project {
             this.levelTimer.Restart();
         }
 
-        public LevelStatus GetStartingLevelStatus(string levelId) {
-            var localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
-            var levelStatus = new LevelStatus();
-
-            return levelStatus;
-        }
-
         public void StopGame() {
             lock (isStartedLock) {
                 this.IsStarted = false;
@@ -299,11 +317,11 @@ namespace Project {
         // Add Red, Green and Blue environment lights with different rate of movements
         private void AddEnvironmentLights() {
             var rate = new Vector3(RandomGenerator.NextFloat(0.001f, 0.005f), RandomGenerator.NextFloat(0.001f, 0.005f), RandomGenerator.NextFloat(0.001f, 0.005f));
-            var red = new Light(this,Color.Red.ToColor4(), 1f, rate);
+            var red = new Light(this, Color.Red.ToColor4(), 1f, rate);
             rate = new Vector3(RandomGenerator.NextFloat(0.001f, 0.005f), RandomGenerator.NextFloat(0.001f, 0.005f), RandomGenerator.NextFloat(0.001f, 0.005f));
-            var green = new Light(this,Color.Green.ToColor4(), 1f, rate);
+            var green = new Light(this, Color.Green.ToColor4(), 1f, rate);
             rate = new Vector3(RandomGenerator.NextFloat(0.001f, 0.005f), RandomGenerator.NextFloat(0.001f, 0.005f), RandomGenerator.NextFloat(0.001f, 0.005f));
-            var blue = new Light(this,Color.Blue.ToColor4(), 1f, rate);
+            var blue = new Light(this, Color.Blue.ToColor4(), 1f, rate);
             Lights.Add(red);
             Lights.Add(green);
             Lights.Add(blue);
